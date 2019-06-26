6f159acd2d7450e26415aba2b695c1f0f35f1bcf
Jashan Saraon <Jashanp.k99@gmail.com>
2019-06-26 10:40:12 -0500

    Make the Domain Classes.

---
+++ Scheduler-App/Models/Domain/Course.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler_App.Models.Domain
+{
+    public class Course
+    {
+    }
+}
---
+++ Scheduler-App/Models/Domain/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler_App.Models.Domain
+{
+    public class Student
+    {
+
+    }
+}
