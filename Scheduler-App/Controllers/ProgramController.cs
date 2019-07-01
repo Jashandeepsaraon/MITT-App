96a98c880f0c3a4a76d5ed29ed8e0e0e27fad457
palwinder sandhu <palwindersandhu367@gmail.com>
2019-07-01 14:07:36 -0500

    Folder Changed

--- Scheduler-App/Controllers/ProgramController.cs
+++ Scheduler-App/Controllers/ProgramController.cs
@@ -18,58 +18,48 @@ namespace Scheduler_App.Controllers
         public ProgramController()
 
         {
-
             DbContext = new ApplicationDbContext();
-
-
         }
-        // GET: Program
+       
         public ActionResult Index()
         {
-            return View();
+            var model = DbContext.ProgramDatabase
+                .Select(p => new CreateEditSchoolProgramViewModel
+                {
+                    Name = p.Name,
+                    StartDate = p.StartDate
+                }).ToList();
+
+            return View(model);
         }
 
         //GET : CreateProgram
         [HttpGet]
-
         [Authorize(Roles = "Admin")]
-
         public ActionResult CreateProgram()
-
         {
             return View();
         }
 
         [HttpPost]
-
         [Authorize(Roles = "Admin")]
-
         public ActionResult CreateProgram (CreateEditSchoolProgramViewModel formData)
-
         {
-
             return SaveProgram(null, formData);
-
         }
-        private ActionResult SaveProgram(int? id, CreateEditSchoolProgramViewModel formData)
 
+        private ActionResult SaveProgram(int? id, CreateEditSchoolProgramViewModel formData)
         {
             if (!ModelState.IsValid)
             {
-
                 return View();
-
             }
+
             Program program;
-
             var userId = User.Identity.GetUserId();
-
             if (!id.HasValue)
-
             {
-
                 program = new Program();
-
                 //var applicationUser = DbContext.Users.FirstOrDefault(user => user.Id == userId);
 
                 //if (applicationUser == null)
@@ -84,77 +74,44 @@ namespace Scheduler_App.Controllers
 
                 DbContext.ProgramDatabase.Add(program);
             }
-
             else
-
             {
                 program = DbContext.ProgramDatabase.FirstOrDefault(p => p.Id == id);
-
                 if (program == null)
-
                 {
-
                     return RedirectToAction(nameof(ProgramController.Index));
-
                 }
-
             }
-
             program.Name = formData.Name;
-
             program.StartDate = DateTime.Now;
-
             DbContext.SaveChanges();
-
             return RedirectToAction(nameof(ProgramController.Index));
-
         }
 
         //GET: EditProgram
         [HttpGet]
-
         [Authorize(Roles = "Admin")]
-
         public ActionResult EditProgram(int? id)
-
         {
-
             if (!id.HasValue)
-
             {
-
                 return RedirectToAction(nameof(ProgramController.Index));
-
             }
-
-
-
             var program = DbContext.ProgramDatabase.FirstOrDefault(p => p.Id == id);
 
-
-
             if (program == null)
-
             {
-
                 return RedirectToAction(nameof(ProgramController.Index));
-
             }
 
             var model = new CreateEditSchoolProgramViewModel();
-
             model.Name = program.Name;
-
             return View(model);
-
         }
         //POST:
         [HttpPost]
-
         [Authorize(Roles = "Admin")]
-
         public ActionResult EditProgram(int id, CreateEditSchoolProgramViewModel formData)
-
         {
             return SaveProgram(id, formData);
         }
