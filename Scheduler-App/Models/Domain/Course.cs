2a17ebcb3ef02e07c1bd6aa1c60f8703afa14aec
Jashan Saraon <Jashanp.k99@gmail.com>
2019-07-02 10:25:20 -0500

    Fix layout of Program and working on Courses.

--- Scheduler-App/Models/Domain/Course.cs
+++ Scheduler-App/Models/Domain/Course.cs
@@ -7,5 +7,17 @@ namespace Scheduler_App.Models.Domain
 {
     public class Course
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TimeSpan ClassStartTime { get; set; }
+        public TimeSpan ClassEndTime { get; set; }
+        public virtual Program SchoolProgram { get; set; }
+        public int SchoolProgramId { get; set; }
+        public virtual ClassRoom ClassRoom { get; set; }
+        public int ClassRoomId { get; set; }
+        public virtual Instructor Instructor { get; set; }
+        public string InstructorId { get; set; }
     }
 }
