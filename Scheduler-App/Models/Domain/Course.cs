cf0bc775fbea04090fb5bbe699a07846881fb282
Jashan Saraon <Jashanp.k99@gmail.com>
2019-07-02 14:55:44 -0500

    working on course controller

--- Scheduler-App/Models/Domain/Course.cs
+++ Scheduler-App/Models/Domain/Course.cs
@@ -9,15 +9,16 @@ namespace Scheduler_App.Models.Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public TimeSpan ClassStartTime { get; set; }
-        public TimeSpan ClassEndTime { get; set; }
+        //public DateTime StartDate { get; set; }
+        //public DateTime EndDate { get; set; }
+        //public TimeSpan ClassStartTime { get; set; }
+        //public TimeSpan ClassEndTime { get; set; }
         public virtual Program SchoolProgram { get; set; }
         public int SchoolProgramId { get; set; }
-        public virtual ClassRoom ClassRoom { get; set; }
-        public int ClassRoomId { get; set; }
+        //public virtual ClassRoom ClassRoom { get; set; }
+        //public int ClassRoomId { get; set; }
         public virtual Instructor Instructor { get; set; }
         public string InstructorId { get; set; }
+        public TimeSpan Hours { get; set; }
     }
 }
--- Scheduler-App/Models/ViewModel/CreateEditCourseViewModel.cs
+++ Scheduler-App/Models/ViewModel/CreateEditCourseViewModel.cs
@@ -1,20 +1,17 @@
-﻿using Scheduler_App.Models.Domain;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Scheduler_App.Models.ViewModel
 {
-    public class CourseViewModel
+    public class CreateEditCourseViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        //public TimeSpan ClassStartTime { get; set; }
-        //public TimeSpan ClassEndTime { get; set; }
-        //public virtual ClassRoom ClassRoom { get; set; }
-        //public int ClassRoomId { get; set; }
         public SelectList Programs { get; set; }
         public SelectList Instructors { get; set; }
         public TimeSpan Hours { get; set; }
