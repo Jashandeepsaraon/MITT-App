090ea81d8520aa7ca59ce6564a0776107e005879
Jashan Saraon <Jashanp.k99@gmail.com>
2019-07-16 15:40:28 -0500

    Try to add Instructors in Course.

--- Scheduler-App/Models/Domain/Program.cs
+++ Scheduler-App/Models/Domain/Program.cs
@@ -11,7 +11,7 @@ namespace Scheduler_App.Models.Domain
         public string Name { get; set; }
         //public virtual List<Instructor> Instructors { get; set; }
         //public virtual List<Student> Students { get; set; }
-        //public virtual List<Course> Courses { get; set; }
+        public virtual List<Course> Courses { get; set; }
         //public int StudentId { get; set; }
         public DateTime StartDate { get; set; }
 
